0f39b1d2565bb9e48b2ffd01d97f5850df3a90af
mohammed shaheed sm <144881473+mohammedshaheed1@users.noreply.github.com>
2024-12-10 22:32:24 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 5174,
     proxy: {
-      '/api': 'https://forever-ecommerce-ahhk.onrender.com', // Forward /api requests to backend
+      '/api': 'https://forever-frontend-hvhf.onrender.com', // Forward /api requests to backend
     }
   }
 })
