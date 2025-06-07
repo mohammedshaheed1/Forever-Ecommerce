cadbb5e9513e15dfa5aafb5e5b68025254332411
shaheed <shaheed.alltechnician@gmail.com>
2025-06-07 16:38:55 +0530

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5174,
-    proxy: {
-      '/api': 'https://forever-ecommerce-ahhk.onrender.com', // Forward /api requests to backend
-    }
   }
 })
